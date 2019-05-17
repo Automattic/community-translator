d230c5f45686be503b6ef24fc72213ee23527c5a
Julian de Bhal <julian.debhal@automattic.com>
2019-05-17 19:40:10 +1000

    Produce UMD library (like uglify did)

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ module.exports = {
   mode: 'production',
   entry: './lib/index.js',
   output: {
+	library: 'communityTranslator',
+	libraryTarget: 'umd',
 	path: path.resolve( __dirname ),
     filename: 'community-translator.min.js'
   },
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -4,6 +4,8 @@ module.exports = {
   mode: 'development',
   entry: './lib/index.js',
   output: {
+	library: 'communityTranslator',
+	libraryTarget: 'umd',
 	path: path.resolve( __dirname ),
     filename: 'community-translator.js'
   },
